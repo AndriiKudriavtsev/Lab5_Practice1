91d728196c6942c404daf79f6f3791d9e0ea16ec
AndriiKudriavtsev <qwerdf0921@gmail.com>
2025-04-08 15:24:51 +0200

    Tutorial finished

--- MultithreadingAndAsynchronousTutorial/MultithreadingTutorial/Program.cs
+++ MultithreadingAndAsynchronousTutorial/MultithreadingTutorial/Program.cs
@@ -1,1 +1,57 @@
-﻿Console.WriteLine("Hello multithreading!");
+﻿//void LongRunningTask()
+//{
+//    var list = new List<int>();
+
+//    for (var i = 0; i < 1_000_000_000; i++)
+//    {
+//        if (i > 0 && i % 100_000_000 == 0)
+//            Console.WriteLine($"{Thread.CurrentThread.Name}: {i} was reached");
+
+//        list.Add(i);
+//    }
+
+//    Console.WriteLine($"{Thread.CurrentThread.Name}: The job is completed");
+//}
+
+//var t1 = new Thread(LongRunningTask);
+//t1.Name = "Thread 1";
+//t1.IsBackground = true;
+
+//var t2 = new Thread(LongRunningTask);
+//t2.Name = "Thread 2";
+//t2.IsBackground = true;
+
+//t1.Start();
+//t2.Start();
+//Console.WriteLine("The worked thread is started");
+
+//t1.Join();
+//t2.Join();
+//Console.WriteLine("The program ends");
+
+var counter = 0;
+var dummy = new object();
+
+var t1 = Task.Run(Increment);
+var t2 = Task.Run(Increment);
+
+Task.WaitAny(t1, t2);
+
+Console.WriteLine($"The value of counter is {counter}");
+return;
+
+
+void Increment()
+{
+    for(var i = 0; i < 1_000_000_000; i++)
+    {
+        if (i > 0 && i % 100_000_000 == 0)
+            Console.WriteLine($"{i} was reached");
+        lock (dummy)
+        {
+            counter++;
+        }
+    }
+
+    Console.WriteLine("The job is completed");
+}
